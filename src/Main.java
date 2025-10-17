8288dbc0b5d324a1ad9e454d51362a572047a402
Korinnyi Zakhar <zgk2006@gmail.com>
2025-10-17 13:35:09 +0300

    New changes in Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,41 +14,45 @@ public class Main {
         Animal humming = AnimalFactory.createAnimal("hummingbird", "Вжик", 1, Gender.FEMALE, 57);
         Animal croc = AnimalFactory.createAnimal("crocodile", "Зубастик", 18, Gender.MALE, 4.8);
 
-        // Додаються тварин у відповідні вольєри (Агрегація)
+        // Додаються тварин у вольєри (Агрегація)
         lionEnclosure.addAnimal(simba);
         birdEnclosure.addAnimal(humming);
         crocodileEnclosure.addAnimal(croc);
 
-        // 5. Додаються вольєри до зоопарку
+        // Додаються вольєри до зоопарку
         zoo.addEnclosure(lionEnclosure);
         zoo.addEnclosure(birdEnclosure);
         zoo.addEnclosure(crocodileEnclosure);
 
         System.out.println();
 
-        // 6. Демонстрація GRASP "Інформаційний експерт" та Агрегації
+        // Демонстрація GRASP "Інформаційний експерт" та Агрегації
         zoo.listAllEnclosures();
 
-        // 7. Створюються та наймаюються працівники
+        // Створюються та наймаються працівників
         Zookeeper john = new Zookeeper("Віктор", 12200.0);
         Veterinarian mary = new Veterinarian("Артем", 18000.0);
-        zoo.hireEmployee(john);
-        zoo.hireEmployee(mary);
 
-        // 8. Створюються та додаються відвідувачі
+        // Менеджер працівників
+        zoo.getEmployeeManager().hireEmployee(john);
+        zoo.getEmployeeManager().hireEmployee(mary); // Виклик через менеджер
+
+        // 8. Створюються та додаються відвідувачів
         Visitor visitor1 = new Visitor("Вікторія", 20, Gender.FEMALE);
         Visitor visitor2 = new Visitor("Петро", 21, Gender.MALE);
-        zoo.addVisitor(visitor1);
-        zoo.addVisitor(visitor2);
+
+        // Менеджер відвідувачів
+        zoo.getVisitorManager().addVisitor(visitor1);
+        zoo.getVisitorManager().addVisitor(visitor2);
 
         System.out.println();
 
-        // 9. Демонстрація поліморфізму працівників
-        zoo.runDailyRoutine();
+        // Менеджер працівників використовується для щоденної роботи
+        zoo.getEmployeeManager().runDailyRoutine();
 
         System.out.println();
 
-        // 10. Приклади взаємодії
+        // 9. Приклади взаємодії
         john.feedAnimal(simba);
         mary.healAnimal(simba);
 
@@ -56,7 +60,6 @@ public class Main {
 
         visitor1.buyTicket();
         visitor1.watchAnimal(croc);
-
 
 
     }
